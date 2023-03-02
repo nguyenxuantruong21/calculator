291ee1eb7af69aafb0ed32d3197abcd76f66c5c7
Nguyen Truong <xuantruong200101@gmai.com>
2023-03-02 18:49:37 +0700

    nguyen xuan truong

---
+++ ex04_cacl.js
@@ -0,0 +1,31 @@
+
+// lay ra INPUT va Item
+var input = document.querySelector('.input')
+var items = Array.from(document.querySelectorAll('.item'))
+
+// ham duyet cac phan tu co class: item
+items.forEach(function (btn) {
+    btn.addEventListener('click', function () {
+        if (input.innerHTML === '0')
+            input.innerHTML = '0'
+        // neu an clear all thi xoa tat ca
+        if (btn.innerHTML === 'C')
+            input.innerHTML = '0'
+        else if (btn.innerHTML === 'CE') {
+            // neu xoa tung phan tu thi xoa o cuoi len
+            var arrText = Array.from(input.innerHTML)
+            arrText.splice(arrText.length - 1, 1)
+            // neu do dai > 0 thi noi thanh chuoi
+            if (arrText.length !== 0) {
+                input.innerHTML = arrText.join('')
+            } else {
+                input.innerHTML = '0'
+            }
+        } else if (btn.innerHTML === '=') {
+            // thuc hien cac phep toan
+            input.innerHTML = eval(input.innerHTML)
+        } else {
+            input.innerHTML += btn.innerHTML
+        }
+    })
+})
